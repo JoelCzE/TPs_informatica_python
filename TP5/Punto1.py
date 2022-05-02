381ac180c723cb6a0ee523667a4a848ff657896d
joel <joelce998@gmail.com>
2022-05-02 00:45:49 -0300

    Punto1 terminado

--- TP5/Punto1.py
+++ TP5/Punto1.py
@@ -1,17 +1,27 @@
 #TP5 Punto 1 Cruz Joel
 
+from email.mime import base
 from os import system
+from re import A
 from time import sleep, time
 
 system("cls")
 
 #Declaracion de variables y funciones
 
+def calc_area (x, t):
+    print("El area de la figura es:", x * t)
+
+def calc_perimetro (a, b):
+    print("El perimetro es:", a + a + b + b)
 
 rta= "si"
 while rta != "no":
     #Programa principal
-    
+    base= float( input("Ingrese el valor de la base: "))
+    altura= float( input("Ingrese el valor de la altura: "))
+    rta_area= calc_area(base, altura)
+    rta_perimetro= calc_perimetro(base, altura)
 
     rta= 0
     while rta != "no" and rta != "si":
